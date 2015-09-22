be04b3850d9e365ae0c68e02bd7c145ec6bce0c5
Ironsongming <597981586@qq.com>
2015-09-22 14:16:07 +0800

    update

---
+++ container/circle.py
@@ -0,0 +1,13 @@
+print range(100)
+
+sum = 0
+for x in range(10):
+	sum  += x
+
+print sum
+
+i = 0
+while i < 100 :
+	sum += i
+	i += 1
+print sum
---
+++ container/condition.py
@@ -0,0 +1,10 @@
+#-*-  coding: utf-8 -*-
+
+input = input("请输入:")
+str = '你输入的是%s'
+if int(input) == 90:
+	print str % (90)
+elif int(input) == 80:
+	print str % (80)
+else :
+	print "error"
